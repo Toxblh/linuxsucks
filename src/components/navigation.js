10fb761ad4e757c802850dbc1c3b390e38f7aca8
Innsmouth-trip <invisible-sleeper@yandex.ru>
2020-03-22 04:22:03 +0000

    add logo to navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
+import Logo from './linuxsuckslogo.png'
 
 export default () => (
-  <nav role="navigation">
+  <nav role="navigation" className={styles.navigationwrap}>
+    <img className={styles.navigationlogo} src={Logo} alt="Linux Friday Shitposting"></img>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link to="/">Главная</Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link to="/blog/">Статьи</Link>
       </li>
     </ul>
   </nav>
